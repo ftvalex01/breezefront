fd50c2343d9afb639dc564484f0d3ab610a7a91d
ftvalex01 <ftvalex@hotmail.com>
2024-01-28 13:21:17 +0000

    cambiando config

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://backend-a28.onrender.com',
   headers: {
     'X-Requested-With': 'XMLHttpRequest'
   },
